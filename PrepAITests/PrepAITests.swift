5e1f5d76f6d54fd467f76d01ce083d37a7a41d42
Lin Thit Khant <linthitkhant7@gmail.com>
2026-06-02 19:40:00 +0700

    Initial Commit

---
+++ PrepAITests/PrepAITests.swift
@@ -0,0 +1,19 @@
+//
+//  PrepAITests.swift
+//  PrepAITests
+//
+//  Created by Lin Thit Khant on 2/6/2569 BE.
+//
+
+import Testing
+@testable import PrepAI
+
+struct PrepAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
