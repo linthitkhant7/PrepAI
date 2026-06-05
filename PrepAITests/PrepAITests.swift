846ca1b1719b20785dad6ee7f2eb4e090e74af76
Lin Thit Khant <linthitkhant7@gmail.com>
2026-06-05 15:00:00 +0700

    Add CategoryType tests

--- PrepAITests/PrepAITests.swift
+++ PrepAITests/PrepAITests.swift
@@ -10,10 +10,10 @@ import Testing
 
 struct PrepAITests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        // Swift Testing Documentation
-        // https://developer.apple.com/documentation/testing
+    @Test func categoryTypeHasAllCases() {
+        #expect(CategoryType.systemDesign.rawValue == "systemDesign")
+        #expect(CategoryType.behavioral.rawValue == "behavioral")
+        #expect(CategoryType.iOSSpecific.rawValue == "iOSSpecific")
     }
 
 }
